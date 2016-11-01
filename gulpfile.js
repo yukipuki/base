86ce82b7644ca0a433c760c4a7159e500873b648
Dawid Sappich <dawidsappich@gmail.com>
2016-11-01 21:23:23 +0100

    Add gulp watch task and jquery

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,26 @@
 'ust strict';
 
 var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+var tsPattern = 'app/**/*.ts';
+var tsRootDir = './app';
+var jsOutDir = './dist';
+
+var options = {
+	target: 'ES5',
+	module: 'commonjs',
+	rootDir: tsRootDir,
+	outDir: jsOutDir,
+	moduleResolution : 'node'
+}
+
+gulp.task('compileTypeScript', () => {
+	gulp.src(tsPattern)
+		.pipe(ts(options))
+		.pipe(gulp.dest(jsOutDir));
+});
+
+gulp.task('watchTsFiles', () => {
+	gulp.watch(tsPattern, ['compileTypeScript']);
+});
